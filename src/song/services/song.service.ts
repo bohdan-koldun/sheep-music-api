0feea67f6d64085f3d7cba1df48480bf03d9a941
Bohdan Koldun <koldunbohdan@gmail.com>
2019-09-10 22:19:24 +0300

    changeSlugs

--- src/song/services/song.service.ts
+++ src/song/services/song.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, Logger } from '@nestjs/common';
 import { Connection, Repository, Like } from 'typeorm';
+import { slugify } from 'transliteration';
 import { Song } from '../entities/song.entity';
 import { SongDTO } from '../dto';
 import { PaginationOptionsInterface, Pagination } from '../../pagination';
+import { randomBytes } from 'crypto';
 
 @Injectable()
 export class SongService {
@@ -65,5 +67,45 @@ export class SongService {
         }
 
         return order;
+    }
+
+    async changeSlugs() {
+        let result = [];
+        let page = 0;
+
+        do {
+            result = await this.songRepo
+                .createQueryBuilder('song')
+                .take(200)
+                .skip(page * 200)
+                .getMany();
+
+            await Promise.all(
+                result.map(
+                    async (song) => {
+                        try {
+                            if (song && song.title) {
+                                song.slug = slugify(song.title);
+                                await this.songRepo.save(song);
+                            }
+
+                        } catch (error) {
+                            try {
+                                if (song && song.title) {
+                                    song.slug = slugify(song.title) + Date.now();
+                                    await this.songRepo.save(song);
+                                }
+                            } catch (error) {
+                                Logger.error(error.message, error);
+                            }
+                        }
+                    },
+                ),
+            );
+            page++;
+
+        } while (result && result.length);
+
+        return 'done change slugs';
     }
 }
