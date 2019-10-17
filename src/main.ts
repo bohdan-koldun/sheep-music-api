fec6770d6d05e4bac78391cd10a8ba9eff94cfc7
Bohdan Koldun <koldunbohdan@gmail.com>
2019-10-17 20:25:29 +0300

    options for cors

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,16 @@ import { ConfigService } from 'nestjs-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  app.enableCors(options);
   const configService: ConfigService = app.get(ConfigService);
   await app.listen(configService.get('app.port'));
-  app.enableCors();
+
 }
 bootstrap();
