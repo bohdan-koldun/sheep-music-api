04769c5418360e4d3769607d46ef8227c759edba
Bohdan Koldun <koldunbohdan@gmail.com>
2020-01-30 00:53:09 +0200

    fix: filter

--- src/common/filters/typeorm.order.filter.ts
+++ src/common/filters/typeorm.order.filter.ts
@@ -5,13 +5,13 @@ function generateOrderFilter(filter: string, entity: string): any {
         case 'alphabet':
             return {[`${entity}.title`]: 'ASC'};
         case 'newest':
-            return {[`${entity}.createdAt`]: 'ASC'};
+            return {[`${entity}.createdAt`]: 'DESC'};
         case 'popular':
             return {[`${entity}.viewCount`]: 'DESC'};
         case 'favorite':
             return {[`${entity}.favorite`]: 'DESC'};
         default:
-            return {[`${entity}.createdAt`]: 'ASC'};
+            return {[`${entity}.createdAt`]: 'DESC'};
     }
 }
 
